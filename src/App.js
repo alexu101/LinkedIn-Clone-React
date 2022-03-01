663df9e2c7d98886b398f06fef901cfd7cd1cc68
Tanase Alex <alextanase60@yahoo.com>
2022-03-01 12:19:08 +0200

    login button and register button done

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
 
   return (
     <div className="App">
-      Header
       <Header />
       {!user ? (<Login />)
         : (
--- src/Login.js
+++ src/Login.js
@@ -36,6 +36,20 @@ function Login() {
 
     const loginToApp = (event) => {
         event.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then((userAuth) => {
+                dispatch(
+                    login({
+                        email: userAuth.user.email,
+                        uid: userAuth.user.uid,
+                        displayName: userAuth.user.displayName,
+                        profileUrl: userAuth.user.photoURL,
+                    })
+                );
+            })
+            .catch(error => alert(error));
     };
 
     return (
